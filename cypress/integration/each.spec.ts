ac11aa392a735e068eb4314c41e41ab8afcda14a
Murat Ozcan <muratkerem@gmail.com>
2022-01-13 09:49:02 -0600

    added cypress-each

---
+++ cypress/integration/each.spec.ts
@@ -0,0 +1,67 @@
+import spok from 'cy-spok'
+import { datatype, address } from '@withshepherd/faker'
+
+describe('Crud operations using data session', () => {
+  let token
+
+  const pizzaId = datatype.number()
+  const editedPizzaId = +pizzaId
+  const postPayload = { pizza: pizzaId, address: address.streetAddress() }
+  const putPayload = {
+    pizza: editedPizzaId,
+    address: address.streetAddress()
+  }
+
+  // the common properties between the assertions
+  const commonProperties = {
+    address: spok.string,
+    orderId: spok.test(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/), // regex pattern to match any id
+    status: (s) => expect(s).to.be.oneOf(['pending', 'delivered'])
+  }
+
+  // common spok assertions between put and get
+  const satisfyAssertions = spok({
+    pizza: editedPizzaId,
+    ...commonProperties
+  })
+
+  before(() => cy.task('token').then((t) => (token = t)))
+
+  // pleas use data-driven tests sparingly so that Murat doesn't have to pay AWS a fortune
+  // describe.each(['red', 'green', 'blue'])('color %s', (color) => {
+  // describe.each(['A', 'B', 'C', 'D'])('letter %s', (letter) => {
+  it.each(Cypress._.range(0, 5))('Crud operations with cy spok %k', () => {
+    cy.maybeCreateOrder('orderSession', token, postPayload)
+
+    cy.getOrders(token)
+      .should((res) => expect(res.status).to.eq(200))
+      .its('body')
+      .then((orders) => {
+        const ourPizza = Cypress._.filter(
+          orders,
+          (order) => order.pizza === pizzaId
+        )
+        cy.wrap(ourPizza.length).should('eq', 1)
+        const orderId = ourPizza[0].orderId
+
+        cy.getOrder(token, orderId)
+          .its('body')
+          .should(
+            spok({
+              pizza: pizzaId,
+              ...commonProperties
+            })
+          )
+
+        cy.updateOrder(token, orderId, putPayload)
+          .its('body')
+          .should(satisfyAssertions)
+
+        cy.getOrder(token, orderId).its('body').should(satisfyAssertions)
+
+        cy.deleteOrder(token, orderId).its('status').should('eq', 200)
+      })
+  })
+  // })
+  // })
+})
