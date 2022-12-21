6c8fecd3394957311a88b06c6a17529ca4407f6e
Murat Ozcan <muratkerem@gmail.com>
2022-12-21 11:06:46 -0600

    try chai.config.truncateThreshold

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,3 +4,5 @@ import 'cypress-each'
 // import '@bahmutov/cy-api'
 import 'cypress-plugin-api' // toggle between cy-api and cypress-plugin-api
 import '@replayio/cypress/support'
+
+chai.config.truncateThreshold = 200 // test if we can see more than [object Object] in replay tests
